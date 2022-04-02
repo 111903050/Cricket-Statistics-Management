b2cca61b2a397071d3ec61cea7e8a4da4da81c1c
ritikwahane <ritikwahane335@gmail.com>
2022-04-02 16:09:43 +0530

    league and league_type insert functionality added

---
+++ cricket/backend/routes/league.js
@@ -0,0 +1,45 @@
+const mysql = require("mysql");
+const config = require("../config");
+const router = require("express").Router();
+
+const db = mysql.createPool(config.mysql);
+
+router.get("/", (req, res) => {
+    var query =
+        "SELECT League.league_id as league_id, League.season as season, League_Type.league_name as league_name from League join League_Type on League.league_type_id=League_Type.league_type_id;";
+    db.query(query, (err, result) => {
+        if (err) {
+            return res.status(400).send(err.message);
+        }
+        return res.status(200).send(result);
+    });
+});
+
+router.post("/", (req, res) => {
+    const {
+        league_type_id,
+        startdate,
+        enddate,
+        number_of_teams,
+        country,
+        season,
+        winner,
+    } = req.body;
+    
+    //milliseconds
+    const oneDay = 24 * 60 * 60 * 1000;
+    const duration = Math.round(
+        Math.abs((new Date(startdate) - new Date(enddate)) / oneDay)
+    );
+    var query = `INSERT INTO League (league_type_id, startdate, enddate, duration, number_of_teams, country, season, winner) \
+    VALUES (${league_type_id}, "${startdate}", "${enddate}", ${duration}, ${number_of_teams}, "${country}", ${season}, ${winner})`;
+
+    db.query(query, (err, result) => {
+        if (err) {
+            return res.status(400).send(err.message);
+        }
+        return res.status(200).send(result);
+    });
+});
+
+module.exports = router;
---
+++ cricket/backend/routes/league_type.js
@@ -0,0 +1,28 @@
+const mysql = require("mysql");
+const config = require("../config");
+const router = require("express").Router();
+
+const db = mysql.createPool(config.mysql);
+
+router.get("/", (req, res) => {
+    var query = "SELECT * from League_Type";
+    db.query(query, (err, result) => {
+        if (err) {
+            return res.status(400).send(err.message);
+        }
+        return res.status(200).send(result);
+    });
+});
+
+router.post("/", (req, res) => {
+    const { league_format, league_name, league_logo_link } = req.body;
+    var query = `INSERT INTO League_Type (league_format, league_name, league_logo_link) VALUES ("${league_format}", "${league_name}", "${league_logo_link}")`;
+    db.query(query, (err, result) => {
+        if (err) {
+            return res.status(400).send(err.message);
+        }
+        return res.status(200).send(result);
+    });
+});
+
+module.exports = router;
