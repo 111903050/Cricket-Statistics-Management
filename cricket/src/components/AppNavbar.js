8c07d53aea21b6a08cffcc7f9011d0ff6e3a9b06
ritikwahane <ritikwahane335@gmail.com>
2022-04-04 14:58:28 +0530

    added home page

---
+++ cricket/src/components/AppNavbar.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import HomePage from './HomePage';
+import './Styles.css'
+import {
+    Collapse,
+    Navbar,
+    NavbarBrand, 
+    NavbarToggler,
+    Nav,
+    NavItem,
+    Container
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            isOpen: false,
+        }
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
+    render(){
+        return(
+            <Router>
+                <Navbar color="dark" dark expand="sm" className="mb-5" fixed="top">
+                    <Container>
+                        <NavbarBrand>CricStats</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar> 
+                                <NavItem>
+                                    <Link className="linkclass" to="/">Home</Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link className="linkclass" to="/Search">Search</Link>
+                                </NavItem>
+                                <NavItem>
+                                    <AddPlayer></AddPlayer>
+                                </NavItem> 
+                                <NavItem>
+                                    <AddTeam />
+                                </NavItem>    
+                            </Nav> 
+                        </Collapse>
+                    </Container>
+                </Navbar>
+                <Switch>  
+                    <Route path="/Search" exact component={Search}/>
+                    <Route path="/Search/:team_name" component={Team_Seasons}/>  
+                    <Route path="/" exact component={HomePage}/>
+                    <Route path="/:league" exact component={League_Seasons}/>
+                    <Route path={`/:league/:season`} exact component={Season_Details}/>
+                    <Route path={`/:league/:season/:team`} component={TeamDetails}/>                             
+                </Switch>                                 
+            </Router>
+        )
+    }
+}
+
+export default AppNavbar
---
+++ cricket/src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {Container} from 'reactstrap'
+
+class HomePage extends Component {
+    render(){
+        return(
+            <Container className="toplookout">
+            
+            </Container>
+        )
+    }
+}
+
+export default HomePage
